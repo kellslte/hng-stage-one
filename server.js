0d22b39ee6458af279cbaa62fe2419e50f1d6380
Favour Max-Oti <maxotif@gmail.com>
2023-09-07 17:10:04 +0100

    finished the application

--- server.js
+++ server.js
@@ -1,25 +1,44 @@
 // import your dependencies
-const express = require( 'express' );
+const express = require("express");
+const dotenv = require("dotenv");
 
 // create your handlers
 const app = express();
+dotenv.config();
 
 // assign your middleware
-app.get( '/api', ( req, res, next ) =>
-{ 
-    const { slack_name, track } = req.params;
+app.get("/api", (req, res, next) => {
+  const { slack_name, track } = req.query;
 
-    const currentDay = new Date().getDay();
-    const utcTime = new Date();
+  const daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    return res.status( 200 ).json( {
-        "slack_name": slack_name,
-        "current_day": currentDay,
-        "track": track,
-        "utc_time": utcTime,
-        "github_file_url": "",
-        "github_repo_url": "",
-    } );
-} );
+  const currentDay = daysOfTheWeek[new Date().getDay()];
+  const utcTime = new Date();
+
+  return res.status(200).json({
+    slack_name: slack_name,
+    current_day: currentDay,
+    utc_time: utcTime,
+    track: track,
+    github_file_url:
+      "https://github.com/kellslte/hng-stage-one/blob/main/server.js",
+    github_repo_url: "https://github.com/kellslte/hng-stage-one",
+    status_code: 200,
+  });
+});
+
+// assign the port variable
+const port = process.env.PORT;
 
 // start the server
+app.listen(parseInt(port, 10), () =>
+  console.info(`Starting application on port:${port}`)
+);
